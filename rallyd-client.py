5d057df67ae70c07435e7470dcb0147c6135432a
Dmitry Kalashnik <dkalashnik@mirantis.com>
2015-07-27 21:32:10 +0300

    Add initial versions of rallyd and rallyd-client

---
+++ rallyd-client.py
@@ -0,0 +1,170 @@
+#!/usr/bin/python
+
+import os
+import argparse
+import base64
+import json
+import urlparse
+import pprint
+
+import requests
+
+
+class HTTPClient(object):
+    def __init__(self, base_url=None):
+        self.base_url = base_url
+        self.session = requests.Session()
+
+    def set_base_url(self, base_url):
+        self.base_url = base_url
+
+    def request(self, url, method, headers=None, body=None, **kwargs):
+        if headers is None:
+            headers = {'Content-Type': 'application/json'}
+        r = requests.request(method, urlparse.urljoin(self.base_url, url),
+                             headers=headers, data=body,
+                             **kwargs)
+        return r.headers, json.loads(r.content)
+
+    def post(self, url, body, headers=None, **kwargs):
+        return self.request(url, "POST", headers, body=body, **kwargs)
+
+    def get(self, url, headers=None, **kwargs):
+        return self.request(url, "GET", headers, **kwargs)
+
+    def get_raw(self, url, headers=None, **kwargs):
+        return requests.request("GET", self.base_url + url, **kwargs)
+
+
+class RallydClient(HTTPClient):
+    def recreate_db(self):
+        return self.post("/db", body=json.dumps({}))
+
+    def deployment_create(self, auth_url, endpoint, username,
+                          password, tenant_name, from_env,
+                          *args, **kwargs):
+        body = {"OS_AUTH_URL": auth_url,
+                "OS_ENDPOINT": endpoint,
+                "OS_USERNAME": username,
+                "OS_PASSWORD": password,
+                "OS_TENANT_NAME": tenant_name}
+
+        if from_env:
+            body = dict((key, os.environ.get(key)) for key in body)
+
+        return self.post("/deployments", json.dumps(body))
+
+    def scenario_create(self, scenario_file, scenario_type,
+                        name=None, filename=None, *args, **kwargs):
+        with file(scenario_file) as scenario:
+            data = base64.b64encode(scenario.read())
+        body = {"data": data,
+                "type": scenario_type,
+                "filename": filename,
+                "name": name}
+        return self.post("/scenarios",
+                         body=json.dumps(body))
+
+    def scenarios_list(self, *args, **kwargs):
+        return self.get("/scenarios")
+
+    def task_add(self, scenario_id, *args, **kwargs):
+        return self.post("/tasks",
+                         body=json.dumps({"scenario_id": scenario_id}))
+
+    def task_list(self, *args, **kwargs):
+        return self.get("/tasks")
+
+    def task_get(self, task_id, *args, **kwargs):
+        return self.get("/tasks/{0}".format(task_id))
+
+    def run_create(self, task_ids, *args, **kwargs):
+        return self.post("/runs", body=json.dumps({"task_ids": task_ids}))
+
+    def run_list(self, *args, **kwargs):
+        return self.get("/runs")
+
+    def run_get(self, run_id, *args, **kwargs):
+        return self.get("/runs/{0}".format(run_id))
+
+    def task_result_download(self, filename):
+        return self.get_raw("/result/{0}".format(filename), stream=True)
+
+    def run_result_download(self, run_id, download_dir=".",
+                            *args, **kwargs):
+        resp, results = self.get("/runs/{0}/result".format(run_id))
+        for filename in json.loads(results)["results"]:
+            r = self.task_result_download(filename)
+            path = os.path.join(download_dir, os.path.split(filename)[-1])
+            with open(path, "wb") as result:
+                for block in r.iter_content(1024):
+                    result.write(block)
+        return resp, results
+
+
+if __name__ == "__main__":
+    def add_command(name, func, base_args=None, advanced_args=None):
+        if base_args is None:
+            base_args = []
+
+        if advanced_args is None:
+            advanced_args = {}
+
+        subcommand = command.add_parser(name, description=name)
+        subcommand.set_defaults(func=func)
+
+        for arg in base_args:
+            subcommand.add_argument(arg)
+
+        for args, kwargs in advanced_args:
+            subcommand.add_argument(*args, **kwargs)
+
+    main_parser = argparse.ArgumentParser(prog='PROG')
+    command = main_parser.add_subparsers(title='Commands',
+                                         dest='command',
+                                         metavar='<command>')
+
+    rally = RallydClient()
+
+    main_parser.add_argument("--endpoint", help="rallyd endpoint")
+    main_parser.add_argument("--json", dest="json", action="store_true",
+                             help="Enable json output")
+
+    add_command("db-recreate", rally.recreate_db)
+
+    add_command("deployment-create", rally.deployment_create,
+                base_args=['--auth-url', '--endpoint', '--username',
+                           '--password', '--tenant_name'],
+                advanced_args=[(('--from_env', ), {'dest': 'from_env',
+                                                   'action': 'store_true'})])
+
+    add_command("scenario-create", rally.scenario_create,
+                base_args=['scenario_file', 'scenario_type',
+                           '--filename', '--name'])
+    add_command("scenario-list", rally.scenarios_list)
+
+    add_command("task-create", rally.task_add,
+                base_args=['scenario_id'])
+    add_command("task-list", rally.task_list)
+    add_command("task-show", rally.task_get,
+                base_args=['task_id'])
+
+    add_command(
+        "run-start", rally.run_create,
+        advanced_args=[(('task_ids', ), {'nargs': '+', 'type': str})])
+    add_command("run-list", rally.run_list)
+    add_command("run-show", rally.run_get,
+                base_args=['run_id'])
+
+    add_command("result", rally.run_result_download,
+                base_args=['run_id', '--download_dir'])
+
+    args = main_parser.parse_args()
+    rally.set_base_url(args.endpoint)
+
+    _, data = args.func(**vars(args))
+
+    if args.json:
+        print json.dumps(data)
+    else:
+        pprint.pprint(data)
---
+++ rallyd.py
@@ -0,0 +1,276 @@
+#!/usr/bin/python
+
+import base64
+import subprocess
+import datetime
+import json
+import uuid
+import thread
+import os
+import time
+import sys
+
+from flask import Flask, request, jsonify, send_from_directory
+from rally import api
+from rally import db
+from rally import plugins
+from rally.cli.commands import task as rally_task_commands
+from oslo_config import cfg
+
+CONF = cfg.CONF
+CONF(sys.argv[1:], project="rally")
+plugins.load()
+ENV_NAME = "haos"
+WORK_DIR = "/tmp/"
+
+
+class Resource(object):
+    fields = []
+    non_serializable = []
+
+    def __init__(self, *args, **kwargs):
+        for field in self.fields:
+            setattr(self, field, kwargs.get(field, None))
+
+        self.id = uuid.uuid4().__str__()
+
+    def __getitem__(self, item):
+        return getattr(self, item)
+
+    def __setitem__(self, key, value):
+        setattr(self, key, value)
+
+    def update(self):
+        pass
+
+    @property
+    def data(self):
+        self.update()
+        return dict((field, getattr(self, field))
+                    for field in self.fields
+                    if field not in self.non_serializable)
+
+
+class Scenario(Resource):
+    fields = ['id', 'name', 'filename', 'type', 'args']
+
+    def __init__(self, *args, **kwargs):
+        super(Scenario, self).__init__(*args, **kwargs)
+
+        self.id = uuid.uuid4().__str__()
+
+        self.filename = self.filename if self.filename is not None\
+            else self.id + ".json"
+        self.filename = os.path.join(WORK_DIR, self.filename)
+
+        with file(self.filename, mode="w") as sc_file:
+            sc_file.write(base64.b64decode(kwargs.get('data')))
+
+
+class Task(Resource):
+    fields = ['id', 'scenario_id', 'task_args',
+              'task', 'input_task', 'state']
+    non_serializable = ['task', 'input_task']
+
+    def __init__(self, *args, **kwargs):
+        super(Task, self).__init__(*args, **kwargs)
+
+        self.scenario_id = kwargs.get('scenario_id')
+        scenario = app.scenarios.get_by_id(self.scenario_id)
+
+        self.input_task = \
+            app.rally_task_commands._load_task(
+                task_file=scenario.filename,
+                task_args=self.task_args)
+        self.rally_task = api.Task.create(ENV_NAME, scenario.type)
+        self.id = self.rally_task['uuid']
+        self.state = "new"
+
+    def update(self):
+        rally_task = db.task_get(self.id)
+        self.state = rally_task['status']
+
+    def start_task(self):
+        api.Task.start(ENV_NAME, self.input_task,
+                       task=self.rally_task,
+                       abort_on_sla_failure=False)
+        self.update()
+
+
+class Run(Resource):
+    fields = ['id', 'task_ids', 'state']
+
+    def __init__(self, *args, **kwargs):
+        super(Run, self).__init__(*args, **kwargs)
+
+        self.state = 'new'
+
+    def start(self):
+        for task_id in self.task_ids:
+            task = app.tasks.get_by_id(task_id)
+            thread.start_new_thread(task.start_task, ())
+            time.sleep(5)
+            task.update()
+        self.state = 'running'
+
+    def update(self):
+        tasks_states = []
+        for task_id in self.task_ids:
+            tasks_states.append(app.tasks.get_by_id(task_id)['state'])
+
+        if all([i == 'finished' for i in tasks_states]):
+            self.state = 'finished'
+
+
+class Result(Resource):
+    fields = ['filename']
+
+    def __init__(self, *args, **kwargs):
+        super(Result, self).__init__(*args, **kwargs)
+
+        task = kwargs.get('task')
+        self.filename = "{0}_{1}_{2}.html".\
+            format(app.scenarios.get_by_id(task.scenario_id).type,
+                   datetime.datetime.now(),
+                   task.id)
+
+        app.rally_task_commands.report(
+            task.id,
+            out=os.path.join(WORK_DIR, self.filename),
+            out_format="html")
+
+
+
+class Container(list):
+    def __init__(self, *args, **kwargs):
+        super(Container, self).__init__(*args, **kwargs)
+
+    def get_by_id(self, id):
+        return filter(lambda elem: elem.id == id, self)[0]
+
+    @property
+    def data(self):
+        return [elem.data for elem in self]
+
+
+class Rallyd(Flask):
+    def __init__(self, *args, **kwargs):
+        super(Rallyd, self).__init__(*args, **kwargs)
+        self.scenarios = Container()
+        self.tasks = Container()
+        self.runs = Container()
+        self.results = Container()
+        self.rally_task_commands = rally_task_commands.TaskCommands()
+
+
+app = Rallyd(__name__)
+
+
+@app.route("/db", methods=['POST'])
+def recreate_db():
+    subprocess.call("rally-manage db recreate".split())
+    return jsonify({"msg": "Db recreated"})
+
+
+@app.route("/deployments", methods=['POST'])
+def create_deployment():
+    request_data = json.loads(request.data)
+    config = {
+        "type": "ExistingCloud",
+        "auth_url": request_data.get("OS_AUTH_URL"),
+        "endpoint": request_data.get("OS_ENDPOINT"),
+        "admin": {
+            "username": request_data.get("OS_USERNAME"),
+            "password": request_data.get("OS_PASSWORD"),
+            "tenant_name": request_data.get("OS_TENANT_NAME")}}
+    api.Deployment.create(config, ENV_NAME)
+    return jsonify(config)
+
+
+@app.route("/scenarios", methods=['POST'])
+def upload_scenario():
+    request_data = json.loads(request.data)
+    scenario = Scenario(**request_data)
+    app.scenarios.append(scenario)
+    return jsonify(scenario.data)
+
+
+@app.route("/scenarios", methods=['GET'])
+def list_scenarios():
+    return jsonify({"scenarios": app.scenarios.data})
+
+
+@app.route("/tasks", methods=['POST'])
+def add_task():
+    request_data = json.loads(request.data)
+    task = Task(**request_data)
+    app.tasks.append(task)
+    return jsonify(task.data)
+
+
+@app.route("/tasks", methods=['GET'])
+def list_tasks():
+    return jsonify({"tasks": app.tasks.data})
+
+
+@app.route("/tasks/<task_id>", methods=['GET'])
+def get_task(task_id):
+    return jsonify(app.tasks.get_by_id(task_id).data)
+
+
+@app.route("/runs", methods=['POST'])
+def start_run():
+    request_data = json.loads(request.data)
+    run = Run(**request_data)
+    app.runs.append(run)
+    run.start()
+    return jsonify(run.data)
+
+
+@app.route("/runs", methods=['GET'])
+def list_runs():
+    return jsonify({"runs": app.runs.data})
+
+
+@app.route("/runs/<run_id>", methods=['GET'])
+def get_run(run_id):
+    return jsonify(app.runs.get_by_id(run_id).data)
+
+
+@app.route("/runs/<run_id>/result", methods=['GET'])
+def get_result(run_id):
+    run = app.runs.get_by_id(run_id)
+    local_result_list = []
+
+    for task_id in run.task_ids:
+        result = Result(task=app.tasks.get_by_id(task_id))
+
+        app.results.append(result)
+        local_result_list.append(result)
+
+    return jsonify({"results": [result.filename
+                                for result in local_result_list]})
+
+
+@app.route("/result/<filename>")
+def get_single_result(filename):
+    return send_from_directory(WORK_DIR, filename)
+
+
+@app.route("/verification", methods=['POST'])
+def install_tempest():
+    pass
+
+
+@app.route("/verification/run", methods=['POST'])
+def run_tempest():
+    pass
+
+
+@app.route("/verification/result", methods=['GET'])
+def get_tempest_results():
+    pass
+
+
+if __name__ == "__main__":
+    app.run("0.0.0.0", 8001, debug=True)
